f554c8732371c26be1e1c326b5c261aea5a41bef
flyking999 <103220762+flyking999@users.noreply.github.com>
2022-04-09 03:22:02 +0800

    Update GitTest.java

--- src/main/java/com/zbj/git/GitTest.java
+++ src/main/java/com/zbj/git/GitTest.java
@@ -4,7 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git test");
         System.out.println("git test master...");
-        System.out.println("git test dev22222!!!!...");
-        System.out.println("git test master modified...");
+        System.out.println("git test master 1...");
+        System.out.println("git test master 2...");
     }
 }
