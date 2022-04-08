167f0dcce33cbb2b1d4515cca5b04cea19d11281
zbj <zbj@sina.com>
2022-04-09 01:39:39 +0800

    dev第1次提交

--- src/main/java/com/zbj/git/GitTest.java
+++ src/main/java/com/zbj/git/GitTest.java
@@ -2,7 +2,7 @@ package com.zbj.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("git test1");
-        System.out.println("git test2");
+        System.out.println("git test");
+        System.out.println("git test dev...");
     }
 }
