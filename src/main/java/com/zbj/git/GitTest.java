7d80341348c9f272c48874c2e5b5196000ecd19b
zbj <zbj@sina.com>
2022-04-09 01:43:02 +0800

    解决冲突提交

--- src/main/java/com/zbj/git/GitTest.java
+++ src/main/java/com/zbj/git/GitTest.java
@@ -3,7 +3,7 @@ package com.zbj.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git test");
-        System.out.println("git test master");
+        System.out.println("git test master...");
         System.out.println("git test dev...");
     }
 }
