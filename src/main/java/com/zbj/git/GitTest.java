d64dfcc09945b42cc5aa4ff9f90601f16e84bf97
zbj <zbj@sina.com>
2022-04-09 03:18:39 +0800

    master修改2

--- src/main/java/com/zbj/git/GitTest.java
+++ src/main/java/com/zbj/git/GitTest.java
@@ -4,7 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git test");
         System.out.println("git test master...");
-        System.out.println("git test dev!!!!...");
+        System.out.println("git test dev22222!!!!...");
         System.out.println("git test master modified...");
     }
 }
